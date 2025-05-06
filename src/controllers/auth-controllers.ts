b5251a4dc251a744c6cc47dc41578728f886181c
josephencila <joseph.mesia.encila@gmail.com>
2025-05-06 21:04:22 +0800

    feat: auth controllers, sign in with oauth

--- src/controllers/auth-controllers.ts
+++ src/controllers/auth-controllers.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express"
 import asyncHandler from "express-async-handler"
 import { browserClient } from "@configs/supabase/browser-client"
-import { signInWithEmailSchema } from "@utils/schemas"
+import { signInWithEmailSchema, signInWithOauthSchema } from "@utils/schemas"
+import { serverClient } from "@configs/supabase/server-client"
 
 
 export const signInWithEmail = asyncHandler(async (req: Request, res: Response) => {
@@ -14,6 +15,28 @@ export const signInWithEmail = asyncHandler(async (req: Request, res: Response) 
 
     const { data, error } = await supabase.auth.signInWithOtp({
         email: result.data.email,
+    })
+
+    if (error) throw new Error(error.message)
+
+    res.status(200).json(data)
+
+})
+
+
+export const signInWithOauth = asyncHandler(async (req: Request, res: Response) => {
+
+    const result = signInWithOauthSchema.safeParse(req.body)
+
+    if (!result.success) throw new Error(result.error.message)
+
+    const supabase = serverClient(req, res)
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: result.data.provider,
+        options: {
+            redirectTo: `${process.env.NODE_PUBLIC_DEV_BASE_URL}/auth/callback`,
+        }
     })
 
     if (error) throw new Error(error.message)
--- src/utils/schemas/index.ts
+++ src/utils/schemas/index.ts
@@ -1,5 +1,12 @@
+import { enumsOauthProvider } from '@utils/enums';
 import { z } from 'zod';
 
 export const signInWithEmailSchema = z.object({
     email: z.string().email(),
+});
+
+
+export const signInWithOauthSchema = z.object({
+    provider: enumsOauthProvider
+        .describe('Supported OAuth providers')
 });
