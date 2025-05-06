b55ce0e07522f3b1e8782b696ff7153c01369823
josephencila <joseph.mesia.encila@gmail.com>
2025-05-06 19:29:32 +0800

    feat: controllers, auth controller

---
+++ src/controllers/auth-controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express"
+
+import { browserClient } from "@configs/supabase/browser-client"
+import { signInWithEmailSchema } from "@utils/schemas"
+
+
+const errorHandler = (err: any, req: Request, res: Response) => {
+    if (err instanceof Error) {
+        return res.status(400).json({ error: err.message })
+    }
+    return res.status(500).json({ error: 'Internal server error' })
+
+}
+
+export const signInWithEmail = async (req: Request, res: Response) => {
+
+    try {
+        const result = signInWithEmailSchema.safeParse(req.body)
+
+        if (!result.success) {
+            throw new Error(result.error.message)
+        }
+
+        const supabase = browserClient()
+
+        const { data, error } = await supabase.auth.signInWithOtp({
+            email: result.data.email,
+        })
+
+        return res.status(200).json(data)
+
+    } catch (error) {
+        errorHandler(error, req, res)
+    }
+}
--- src/utils/schemas/index.ts
+++ src/utils/schemas/index.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
 
-export const emailSchema = z.object({
+export const signInWithEmailSchema = z.object({
     email: z.string().email(),
 });
