66a8e19bb7c8d1c2b3ebd43122f0dc9e79a7906a
josephencila <joseph.mesia.encila@gmail.com>
2025-05-06 21:28:23 +0800

    refactor: auth routes, implement verify otp token

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,12 +1,14 @@
 import express, { Router } from 'express';
 
-import { signInWithEmail, signInWithOauth } from '@controllers/auth-controllers'
+import { signInWithEmail, signInWithOauth, verifyOtpToken } from '@controllers/auth-controllers'
 
 const router: Router = express.Router();
 
 
 router.post('/sign-in/email', signInWithEmail)
 router.post('/sign-in/oauth', signInWithOauth)
+router.post('/sign-in/verify', verifyOtpToken)
+
 
 
 export default router
