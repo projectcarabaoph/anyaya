66aa7ddf1b2210d69485f523b38ff22a88f48a11
josephencila <joseph.mesia.encila@gmail.com>
2025-05-06 21:07:22 +0800

    refactor: auth routes, implement sign in with oauth

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,10 +1,12 @@
 import express, { Router } from 'express';
 
-import { signInWithEmail } from '@controllers/auth-controllers'
+import { signInWithEmail, signInWithOauth } from '@controllers/auth-controllers'
 
 const router: Router = express.Router();
 
 
 router.post('/sign-in/email', signInWithEmail)
+router.post('/sign-in/oauth', signInWithOauth)
+
 
 export default router
