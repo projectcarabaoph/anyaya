e247991ab21476faff7994a6266decfe55c1c0d1
josephencila <joseph.mesia.encila@gmail.com>
2025-05-06 21:45:37 +0800

    refactor: auth routes, impelement callback token

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,14 +1,13 @@
 import express, { Router } from 'express';
 
-import { signInWithEmail, signInWithOauth, verifyOtpToken } from '@controllers/auth-controllers'
+import { callbackToken, signInWithEmail, signInWithOauth, verifyOtpToken } from '@controllers/auth-controllers'
 
 const router: Router = express.Router();
-
 
 router.post('/sign-in/email', signInWithEmail)
 router.post('/sign-in/oauth', signInWithOauth)
 router.post('/sign-in/verify', verifyOtpToken)
-
+router.post('/sign-in/callback', callbackToken)
 
 
 export default router
