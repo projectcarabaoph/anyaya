43b5962bafd9489305639f160e1251e8cba40516
josephencila <joseph.mesia.encila@gmail.com>
2025-05-10 17:15:21 +0800

    chore: setup sonner toast

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'sonner'
+
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster position='top-right' richColors duration={3000} />
     <App />
   </StrictMode>,
 )
