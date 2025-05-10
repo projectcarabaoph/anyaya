afb8ff9fac63dfb9f953816feec604f107335d59
josephencila <joseph.mesia.encila@gmail.com>
2025-05-10 17:17:07 +0800

    chore: setup disable react dev tools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
 import './index.css'
 import App from './App.tsx'
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,3 +16,5 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+
+
