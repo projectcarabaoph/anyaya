52419bc268bdca969d9a331dee4d1fdf401f982c
josephencila <joseph.mesia.encila@gmail.com>
2025-05-10 16:45:40 +0800

    refactor: auth api, created sign with email

--- client/src/api/auth/index.ts
+++ client/src/api/auth/index.ts
@@ -10,3 +10,32 @@
  /_/   \_\___/  |_| |_| |_| /_/   \_\_|  |___|                                         
                                                                                                                                                                                                                                                                                                                                                                                                                                                     
 */
+
+import serverPaths from "@configs/paths/server.paths.config"
+
+
+export const signInWithEmail = async (email: string) => {
+    try {
+        const response = await fetch(serverPaths.auth.signin.email, {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json",
+                Accept: "application/json"
+            },
+            credentials: 'include',
+            body: JSON.stringify({ email })
+        })
+
+        const data = await response.json()
+
+        if (!response.ok) {
+            const { messsage } = data
+            throw new Error(messsage)
+        }
+
+        const { accessToken } = data
+        return { accessToken }
+    } catch (error) {
+        if (error instanceof Error) throw new Error(error.message);
+    }
+}
