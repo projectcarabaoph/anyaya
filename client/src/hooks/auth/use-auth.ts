9f253bfeb47ef29b0858506399245390eff00861
josephencila <joseph.mesia.encila@gmail.com>
2025-05-14 22:55:06 +0800

    feat: client, use auth hook

---
+++ client/src/hooks/auth/use-auth.ts
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+
+import AuthContext from "@contexts/auth-context"
+
+const useAuth = () => {
+    const context = useContext(AuthContext)
+    if (!context) console.error('auth hook should be used inside context provider')
+    return context
+}
+
+export default useAuth
