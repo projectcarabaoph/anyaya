f7d809be782785bba22eedc3c86bce02af0b5259
josephencila <joseph.mesia.encila@gmail.com>
2025-05-14 11:03:13 +0800

    refactor: client, added auth client paths

--- client/src/configs/paths/client.paths.config.ts
+++ client/src/configs/paths/client.paths.config.ts
@@ -5,6 +5,13 @@ const pathsSchema = z.string().min(1)
 const schema = z.object({
     marketing: z.object({
         landing: pathsSchema
+    }),
+    auth: z.object({
+        signin: z.object({
+            oauth: pathsSchema,
+            verify: pathsSchema,
+            callback: pathsSchema,
+        }),
     })
 })
 
@@ -12,6 +19,13 @@ const schema = z.object({
 const clientPaths = schema.parse({
     marketing: {
         landing: "/"
+    },
+    auth: {
+        signin: {
+            oauth: "/auth/sign-in",
+            verify: "/auth/sign-in/verify",
+            callback: "/auth/sign-in/callback",
+        },
     }
 } satisfies z.infer<typeof schema>)
 
