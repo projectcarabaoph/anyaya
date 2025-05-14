1ee6a42780ff5d0e2894294de57d35d5c4f0a546
josephencila <joseph.mesia.encila@gmail.com>
2025-05-14 10:56:05 +0800

    feat: client, landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
-import NotFoundPage from "@pages/not-found-page"
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
+
+// Marketing
+import LandingPage from "@pages/marketing/landing-page"
+
+// Common
+import NotFoundPage from "@pages/not-found-page"
 
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
+        <Route path="/" element={<LandingPage />} />
 
         <Route path="*" element={<NotFoundPage />} />
       </Route>
