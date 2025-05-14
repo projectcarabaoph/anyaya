bae24b092239d21fbf1b474d7329fa80f8d5ee46
josephencila <joseph.mesia.encila@gmail.com>
2025-05-14 10:47:45 +0800

    feat: client, not found page

---
+++ client/src/pages/not-found-page.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom"
+
+import clientPaths from "@configs/paths/client.paths.config"
+
+const NotFoundPage = () => {
+    return (
+        <div className='flex w-full min-h-dvh h-auto justify-center items-center '>
+            <div className="flex flex-col items-center justify-center gap-2">
+                <h1>404 Page not found</h1>
+                <NavLink to={clientPaths.marketing.landing} className='bg-slate-400 hover:bg-slate-400/90 p-2 rounded-md text-white w-24 text-center'>Go back</NavLink>
+            </div>
+        </div>
+    )
+}
+
+export default NotFoundPage
