9bf8366a8586b4aebc8f3349c8df283795c6aa67
josephencila <joseph.mesia.encila@gmail.com>
2025-05-14 11:23:45 +0800

    feat: client, sign-in page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,26 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 
+import clientPaths from "@configs/paths/client.paths.config"
+
 // Marketing
 import LandingPage from "@pages/marketing/landing-page"
 
+// Auth
+import SignInPage from "@pages/auth/sign-in-page"
+
 // Common
 import NotFoundPage from "@pages/not-found-page"
+
+
 
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route path="/" element={<LandingPage />} />
+        <Route path={clientPaths.marketing.landing} element={<LandingPage />} />
+        <Route path={clientPaths.auth.signin.oauth} element={<SignInPage />} />
+
 
         <Route path="*" element={<NotFoundPage />} />
       </Route>
