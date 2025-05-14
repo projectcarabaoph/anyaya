a3dd00992ea149c3361d308ac68b3dfea5786193
josephencila <joseph.mesia.encila@gmail.com>
2025-05-14 10:50:59 +0800

    refactor: client, setup react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
-import Sample from "@components/sample"
+import NotFoundPage from "@pages/not-found-page"
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom"
 
 function App() {
 
-  return (
-    <>
-      <Sample />
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
 
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  )
+
+  return (
+    <RouterProvider router={router} />
   )
 }
 
