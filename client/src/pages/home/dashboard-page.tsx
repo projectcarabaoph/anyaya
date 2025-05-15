9c7194a8ffd0af61c6dcb602a32c558322333c8a
josephencila <joseph.mesia.encila@gmail.com>
2025-05-15 20:37:43 +0800

    refactor: client, dashboard implement signout

--- client/src/pages/home/dashboard-page.tsx
+++ client/src/pages/home/dashboard-page.tsx
@@ -1,6 +1,32 @@
+import { useNavigate } from "react-router-dom"
+import { toast } from "sonner"
+
+import { singOutUser } from "@/api/auth"
+import clientPaths from "@/configs/paths/client.paths.config"
+import useAuth from "@/hooks/auth/use-auth"
+import { Button } from "@/components/ui/button"
+
 const DashboardPage = () => {
+
+    const { accessToken } = useAuth()
+    const navigate = useNavigate()
+
+    const handleSignOut = async (accessToken: string) => {
+        try {
+            await singOutUser(accessToken)
+            toast.success('Signed out successfully.');
+            navigate(clientPaths.marketing.landing)
+        } catch (error) {
+            if (error instanceof Error) toast.error(error.message)
+        }
+    }
+
     return (
-        <div>DashboardPage</div>
+        <div className="flex flex-col items-center justify-center w-full h-full bg-red-500 ">
+            <Button onClick={() => handleSignOut(accessToken)} >
+                Sign Out
+            </Button>
+        </div>
     )
 }
 
