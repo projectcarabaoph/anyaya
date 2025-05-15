3ef52467f63da3c1df6b01b4fae563f7057ce125
josephencila <joseph.mesia.encila@gmail.com>
2025-05-15 20:06:52 +0800

    feat: client, hooks use screen size

---
+++ client/src/hooks/use-screen-size.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import type { IScreenSize } from '@hooks/_types';
+
+
+export const useScreenSize = (): IScreenSize => {
+    const [screenSize, setScreenSize] = useState<IScreenSize>({
+        width: 0,
+        height: 0,
+    });
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const handleResize = () => {
+                setScreenSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            };
+
+            handleResize();
+
+            window.addEventListener('resize', handleResize);
+
+            return () => {
+                window.removeEventListener('resize', handleResize);
+            };
+        }
+    }, []);
+
+    return screenSize;
+};
