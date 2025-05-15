611b136360ec24a2f78d24648aad7028580069b0
josephencila <joseph.mesia.encila@gmail.com>
2025-05-15 13:27:27 +0800

    fix: client, absolute path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -22,14 +22,16 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         "@api": path.resolve(__dirname, "./src/api"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
         "@components": path.resolve(__dirname, "./src/components"),
         "@configs": path.resolve(__dirname, "./src/configs"),
         "@contexts": path.resolve(__dirname, "./src/contexts"),
         "@hooks": path.resolve(__dirname, "./src/hooks"),
         "@pages": path.resolve(__dirname, "./src/pages"),
+        "@layouts": path.resolve(__dirname, "./src/layouts"),
         "@routes": path.resolve(__dirname, "./src/routes"),
         "@utils": path.resolve(__dirname, "./src/utils"),
-        "@types": path.resolve(__dirname, "./src/types"),
+        "@types": path.resolve(__dirname, "./src/types")
       },
     },
   }
