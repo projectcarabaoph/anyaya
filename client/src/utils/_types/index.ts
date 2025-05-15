c71e4175706bec3f850bb8dda54d6464b23225cd
josephencila <joseph.mesia.encila@gmail.com>
2025-05-15 13:05:03 +0800

    feat: client, utils constants

---
+++ client/src/utils/_types/index.ts
@@ -0,0 +1,5 @@
+export type TMarketingLayoutNavLinks = {
+    id: number,
+    name: string,
+    path: string
+}
---
+++ client/src/utils/constants/index.ts
@@ -0,0 +1,16 @@
+import clientPaths from '@configs/paths/client.paths.config';
+import type { TMarketingLayoutNavLinks } from '@utils/_types';
+
+
+export const marketingLayoutNavLinks: TMarketingLayoutNavLinks[] = [
+    {
+        id: 1,
+        name: "Home",
+        path: clientPaths.marketing.landing
+    },
+    {
+        id: 1,
+        name: "Sign In",
+        path: clientPaths.auth.signin
+    },
+]
