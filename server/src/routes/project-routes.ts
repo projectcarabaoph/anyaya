0db7cd036bd8e6ecbf960efcf53943d4688d544e
josephencila <joseph.mesia.encila@gmail.com>
2025-05-19 04:59:28 +0800

    feat: server, update project by id router

--- server/src/routes/project-routes.ts
+++ server/src/routes/project-routes.ts
@@ -1,10 +1,10 @@
 import express, { Router } from 'express';
 
-import { createProject } from '@controllers/project-controllers';
+import { createProject, updateProjectById } from '@controllers/project-controllers';
 import { authMiddleware } from '@middlewares/auth-middleware';
 
 const router: Router = express.Router();
 
 router.post('/create', authMiddleware, createProject)
-
+router.post('/update', authMiddleware, updateProjectById)
 export default router
