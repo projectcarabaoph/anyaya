14094ff0f7a9f45a85cfc63d96846c8b8f7c5a52
josephencila <joseph.mesia.encila@gmail.com>
2025-05-18 21:09:25 +0800

    feat: server, create project route

--- server/src/routes/project-routes.ts
+++ server/src/routes/project-routes.ts
@@ -1,8 +1,9 @@
 import express, { Router } from 'express';
 
-
+import { createProject } from '@controllers/project-controllers';
 
 const router: Router = express.Router();
 
+router.post('/create', createProject)
 
 export default router
