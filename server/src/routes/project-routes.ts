08fc2121a235d77aa3a77f606f33e1c246b05d9a
josephencila <joseph.mesia.encila@gmail.com>
2025-05-18 21:17:10 +0800

    refactor: server, added auth middleware in create project route

--- server/src/routes/project-routes.ts
+++ server/src/routes/project-routes.ts
@@ -1,9 +1,10 @@
 import express, { Router } from 'express';
 
 import { createProject } from '@controllers/project-controllers';
+import { authMiddleware } from '@middlewares/auth-middleware';
 
 const router: Router = express.Router();
 
-router.post('/create', createProject)
+router.post('/create', authMiddleware, createProject)
 
 export default router
