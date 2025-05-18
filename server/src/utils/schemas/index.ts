fbb77441d8c181d7dce7ac370df4d6009c09de0e
josephencila <joseph.mesia.encila@gmail.com>
2025-05-19 04:43:12 +0800

    feat: server, update project by id schema

--- server/src/utils/schemas/index.ts
+++ server/src/utils/schemas/index.ts
@@ -37,3 +37,26 @@ export const createProjectSchema = z.object({
         .max(200, { message: "Description must be less than 200 characters." })
         .trim()
 })
+
+
+export const updateProjectByIdSchema = z.object({
+    name: z
+        .string({
+            required_error: "Name is a required field.",
+        })
+        .min(6, { message: "Name must be at least 6 characters." })
+        .max(20, { message: "Name must be less than 20 characters." })
+        .trim(),
+    description: z
+        .string({
+            required_error: "Description is a required field.",
+        })
+        .min(10, { message: "Description must be at least 10 characters." })
+        .max(200, { message: "Description must be less than 200 characters." })
+        .trim(),
+    id: z
+        .union([
+            z.string().uuid({ message: "Invalid ID" }),
+            z.string().min(1, { message: "ID is a required field." })
+        ])
+})
