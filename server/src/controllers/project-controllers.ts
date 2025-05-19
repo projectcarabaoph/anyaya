aff4698bb2bc7011e2d4ea1d80ec0880430f13e0
josephencila <joseph.mesia.encila@gmail.com>
2025-05-19 20:49:23 +0800

    feat: server, delete project by id controller

--- server/src/controllers/project-controllers.ts
+++ server/src/controllers/project-controllers.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 import { serverClient } from "@configs/supabase/server-client";
 import ApiError from "@utils/misc/api-error";
 import errorHandler from "@utils/misc/error-handler";
-import { createProjectSchema, updateProjectByIdSchema } from "@utils/schemas";
+import { createProjectSchema, deleteProjectByIdSchema, updateProjectByIdSchema } from "@utils/schemas";
 
 export const createProject = async (req: Request, res: Response) => {
     try {
@@ -59,4 +59,35 @@ export const updateProjectById = async (req: Request, res: Response) => {
         errorHandler(error, req, res)
         return
     }
-} 
+}
+
+
+export const deleteProjectById = async (req: Request, res: Response) => {
+    try {
+        const result = deleteProjectByIdSchema.safeParse(req.body)
+
+        if (!result.success) throw new ApiError(result.error.errors[0].message, 400)
+
+        const supabase = serverClient(req, res)
+
+        const { id } = result.data
+
+        const { data, error } = await supabase
+            .from('projects')
+            .update({
+                is_deleted: true
+            })
+            .eq('owner_id', req.user?.id)
+            .eq('id', id)
+
+
+        if (error) throw new ApiError(error.message, 400)
+
+        res.status(200).json({ data })
+    } catch (error) {
+        errorHandler(error, req, res)
+        return
+    }
+}
+
+
