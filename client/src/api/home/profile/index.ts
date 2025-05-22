a97a973abf202578111ceaceaabd695da2c0372f
josephencila <joseph.mesia.encila@gmail.com>
2025-05-22 20:36:31 +0800

    feat: client, get profile api

--- client/src/api/home/profile/index.ts
+++ client/src/api/home/profile/index.ts
@@ -10,4 +10,29 @@
                                                              
 */
 
+import serverPaths from "@/configs/paths/server.paths.config"
 
+export const getProfile = async (accessToken: string) => {
+
+    try {
+        const response = await fetch(serverPaths.home.profile.main, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        const data = await response.json()
+
+        if (!response.ok) {
+            const { message } = data
+            throw new Error(message)
+        }
+
+        return data.data
+
+    } catch (error) {
+        if (error instanceof Error) throw new Error(error.message)
+    }
+} 
