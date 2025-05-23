30851d9b3c59f1c98ed38a0ec89348f4f7dc352f
josephencila <joseph.mesia.encila@gmail.com>
2025-05-23 21:41:09 +0800

    feat(client): project api, create project

--- client/src/api/home/project/index.ts
+++ client/src/api/home/project/index.ts
@@ -11,3 +11,33 @@
                                                               
 */
 
+import serverPaths from "@/configs/paths/server.paths.config";
+
+
+export const createProject = async (formData: FormData, accessToken: string) => {
+  const name = formData.get('name') as string;
+  const description = formData.get('description') as string;
+
+  try {
+    const response = await fetch(serverPaths.home.project.create, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${accessToken}`
+      },
+      body: JSON.stringify({ name, description }),
+    });
+
+    const data = await response.json()
+
+    if (!response.ok) {
+      const { message } = data
+      throw new Error(message)
+    }
+
+    return data.data
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message)
+  }
+};
