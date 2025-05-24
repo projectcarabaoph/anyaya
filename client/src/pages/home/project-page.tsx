4d07334eebe8e2604c3383f52c342924027fd3bf
josephencila <joseph.mesia.encila@gmail.com>
2025-05-24 18:10:50 +0800

    refactor(client): diplay project in project page

--- client/src/pages/home/project-page.tsx
+++ client/src/pages/home/project-page.tsx
@@ -1,10 +1,16 @@
-import { getProjectById } from "@/api/home/project";
-import useAuth from "@/hooks/auth/use-auth";
-import { useEffect } from "react";
+
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "sonner";
 
+import { getProjectById } from "@/api/home/project";
+import useAuth from "@/hooks/auth/use-auth";
+import ListComponent from "@/components/shared/list-component";
+
 const ProjectPage = () => {
+
+    const [project, setProject] = useState<IProjectDetails | null>(null)
+
     const { id } = useParams<TParams>();
     const { accessToken } = useAuth();
     const navigate = useNavigate();
@@ -21,7 +27,7 @@ const ProjectPage = () => {
         const fetchProjectById = async () => {
             try {
                 const response = await getProjectById(id, accessToken, signal);
-                console.log(response)
+                setProject(response)
             } catch (error) {
                 if (error instanceof Error && error.name === "AbortError") return; // silently ignore
                 if (error instanceof Error) toast.error(error.message);
@@ -34,8 +40,25 @@ const ProjectPage = () => {
     }, [id, accessToken, navigate]);
 
     return (
-        <div>
-            <span>Projects here</span>
+        <div className="h-full w-full flex flex-col gap-2 bg-orange-200">
+            <div className="flex flex-col gap-2">
+                <span>{project?.name}</span>
+                <span>{project?.description}</span>
+                <span>{project?.created_at}</span>
+                <ListComponent
+                    data={project?.project_memberships as TProjectMembership[]}
+                    as="ul"
+                    renderItem={(member) => (
+                        <li className="flex flex-col gap-2" key={member.added_at}>
+                            <span>{member?.profile_id}</span>
+                            <span>{member?.profile_id}</span>
+                            <span>{member?.role}</span>
+                        </li>
+                    )}
+                >
+
+                </ListComponent>
+            </div>
         </div>
     )
 }
