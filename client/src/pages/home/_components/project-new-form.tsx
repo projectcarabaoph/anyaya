f1e882517c2c416b99a4648bfded0e6a5f70dcdc
josephencila <joseph.mesia.encila@gmail.com>
2025-05-27 21:49:28 +0800

    feat(client): project new form

---
+++ client/src/pages/home/_components/project-new-form.tsx
@@ -0,0 +1,69 @@
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+import { createProjectSchema } from '@/utils/_schemas';
+import clientPaths from '@/configs/paths/client.paths.config';
+
+import useAuth from '@/hooks/auth/use-auth';
+
+import type { TCreateProjectSchema } from '@/utils/_types';
+import { createProject } from '@/api/home/project';
+
+export default function ProjectNewForm() {
+
+    const { accessToken } = useAuth()
+    const navigate = useNavigate()
+
+    const {
+        register,
+        handleSubmit,
+        formState: { isLoading, isDirty, isValid, errors }
+    } = useForm<TCreateProjectSchema>({
+        resolver: zodResolver(createProjectSchema),
+        mode: 'all'
+    })
+
+    const onSubmit = async (formData: TCreateProjectSchema) => {
+        try {
+            await createProject(formData, accessToken)
+            navigate(clientPaths.home.project.dashboard)
+        } catch (error) {
+            if (error instanceof Error) toast.error(error.message)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="bg-white flex flex-col gap-2 w-full max-w-md p-2 rounded-md">
+            <div className='flex flex-col gap-1'>
+                <label htmlFor="name">Project Name</label>
+                <Input  {...register('name')} />
+                {errors.name && (
+                    <span className="text-sm text-red-500">{errors.name.message}</span>
+                )}
+            </div>
+            <div className='flex flex-col gap-1'>
+                <label htmlFor="description">Description</label>
+                <textarea  {...register('description')} />
+                {errors.description && (
+                    <span className="text-sm text-red-500">{errors.description.message}</span>
+                )}
+            </div>
+            <div className="flex flex-col-reverse gap-2 sm:flex-row sm:justify-end">
+                <Button
+                    disabled={isLoading || !isDirty || !isValid}
+                    type="submit"
+                    variant="default"
+                    className="w-full sm:w-auto"
+                >
+                    Create
+                </Button>
+
+            </div>
+        </form>
+    )
+}
--- client/src/pages/home/project-new-page.tsx
+++ client/src/pages/home/project-new-page.tsx
@@ -1,7 +1,10 @@
+import ProjectNewForm from "@/pages/home/_components/project-new-form"
 
 const projectNewPage = () => {
     return (
-        <div>projectNewPage</div>
+        <div className="w-full h-full bg-orange-500 flex justify-center items-center">
+            <ProjectNewForm />
+        </div>
     )
 }
 
