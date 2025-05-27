d0a07a27c4b4f9831c8ad33a9d61b1c6c313d58a
josephencila <joseph.mesia.encila@gmail.com>
2025-05-27 20:00:54 +0800

    feat(client): create project schema

--- client/src/utils/_schemas/index.ts
+++ client/src/utils/_schemas/index.ts
@@ -10,9 +10,45 @@ export const deleteProjectSchema = (projectName: string) =>
     z.object({
         name: z
             .string()
-            .min(1, 'Project Name is required.')
+            .min(1, { message: 'Project Name is required.' })
             .refine((val) => val === projectName, {
                 message: 'Project Name does not match.',
             }),
     });
 
+export const createProjectSchema = z.object({
+    name: z
+        .string()
+        .min(1, { message: 'Project Name is required.' })
+        .superRefine((value, ctx) => {
+            if (value.length <= 6) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: "Project Name must be atleast 6 characters.",
+                });
+            }
+            if (value.length > 40) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: "Project must be no longer than 40 characters.",
+                });
+            }
+        }),
+    description: z
+        .string()
+        .min(1, { message: 'Description is required.' })
+        .superRefine((value, ctx) => {
+            if (value.length <= 10) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: "Description must be atleast 10 characters.",
+                });
+            }
+            if (value.length > 300) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: "Description must be no longer than 300 characters.",
+                });
+            }
+        }),
+}) 
