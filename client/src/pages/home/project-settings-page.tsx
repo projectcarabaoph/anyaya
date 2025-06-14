75c0ebd465289eb1e37d80e819fae5f118e6cf17
josephencila <joseph.mesia.encila@gmail.com>
2025-06-14 13:54:18 +0800

    style(client): project settings page, added title

--- client/src/pages/home/project-settings-page.tsx
+++ client/src/pages/home/project-settings-page.tsx
@@ -2,7 +2,8 @@ import ProjectUpdateForm from "@/pages/home/_components/project-update-form"
 
 const ProjectSettingsPage = () => {
     return (
-        <div className="h-full w-full bg-purple-500 flex justify-center items-center">
+        <div className="h-full w-full bg-purple-500 flex flex-col gap-2 justify-center items-center">
+            <h1>Settings</h1>
             <ProjectUpdateForm />
         </div>
     )
